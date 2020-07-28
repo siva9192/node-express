16073345e5130e3c1c5b2d774b819c23e448833b
siva <barinepallisiva@gmail.com>
2020-07-28 14:38:09 +0530

    templete

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const exphbs =require("express-handlebars");
 const app =express();
 
-let mongodb_url ='mongodb+srv://Siva1996:siva1996@cluster0.4rgwr.mongodb.net/siva1996?retryWrites=true&w=majority';
+
+
+
+//---middlewars start-----
+// ---template engine middleware---*/
+app.engine("handlebars",exphbs());
+app.set("view engine","handlebars");
+
+
+//-----middlewares end-----
+// npm install express -handlebars
+
+let mongodb_url ='mongodb+srv://Siva1996:Siva1996@cluster0.4rgwr.mongodb.net/Siva1996?retryWrites=true&w=majority';
+mongoose.connect(
+    mongodb_url,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    },
+    (err) =>{
+        if (err) throw err;
+        console.log("database conncted !");
+    }
+);
+//error handling
+
+//built-in middleware
+//express built-in
+// app.use(express.static());//to serve static assests sush as html files,images,and so on.
+// app.use(express.json());//
+// //third party middleware
+
 //create express web server
 //basic c route
 app.get('/',(req,res)=>{
-    res.send("app is ready!");
+    // res.send("app is ready!");
+    res.render("./home.handlebars");
 });
 
 let port = 5000;
